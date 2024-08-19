8be4662b55c5bb31332b21e2c2e0460cb1d1ab79
Mickeyoyo <121092107+Mickeyoyo@users.noreply.github.com>
2024-08-19 15:50:11 +0100

    Update Ceasercipher.py
    Added descriptions to some of the lines as required

--- Ceasercipher.py
+++ Ceasercipher.py
@@ -1,7 +1,9 @@
+# A program that can encrypt and decrypt text 
+# Caesar Cipher algorithm. 
 def caesar_cipher_encrypt(text, shift):
     encrypted_text = ""
     
-    # Traverse through each character in the text
+    # Shifts characters based on user input
     for char in text:
         # Encrypt uppercase characters
         if char.isupper():
@@ -32,11 +34,13 @@ def caesar_cipher_decrypt(text, shift):
     
     return decrypted_text
 
+#Allow users to input a message and a shift value
 def main():
     print("Caesar Cipher Program")
     
     # Get user input for encryption or decryption
-    choice = input("Would you like to (E)ncrypt or (D)ecrypt? ").upper()
+    choice = input("What would you like to do? (E)ncrypt or (D)ecrypt: ").upper()
+    #".upper ensures its a upper case"
     
     # Get the message from the user
     message = input("Enter your message: ")
@@ -44,12 +48,14 @@ def main():
     # Get the shift value from the user
     shift = int(input("Enter the shift value: "))
     
+    #A statement to decide what function to invoke
     if choice == 'E':
         encrypted_message = caesar_cipher_encrypt(message, shift)
         print("Encrypted Message: " + encrypted_message)
     elif choice == 'D':
         decrypted_message = caesar_cipher_decrypt(message, shift)
         print(f"Decrypted Message: {decrypted_message}")
+    #Other characters are not defined
     else:
         print("Invalid choice. Please enter 'E' for encryption or 'D' for decryption.")
 
